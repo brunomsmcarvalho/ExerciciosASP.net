fe4c692d006652a04afe2ae0e109748a85fa85ec
Bruno <Bruno@DESKTOP-9J12KR9>
2026-01-31 20:36:54 +0000

    alteracao do exercicio9 pronto para entrega

--- Exercicio9ASP/Controllers/AlunosController.cs
+++ Exercicio9ASP/Controllers/AlunosController.cs
@@ -54,7 +54,7 @@ namespace Exercicio9ASP.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,Email,Idade")] Aluno aluno)
+        public async Task<IActionResult> Create([Bind("Id,Nome,Email,Idade,DataNascimento,Curso")] Aluno aluno)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace Exercicio9ASP.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Email,Idade")] Aluno aluno)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Email,Idade,DataNascimento,Curso")] Aluno aluno)
         {
             if (id != aluno.Id)
             {
--- Exercicio9ASP/Models/Aluno.cs
+++ Exercicio9ASP/Models/Aluno.cs
@@ -14,11 +14,23 @@ namespace Exercicio9ASP.Models
         //Email
         [Required(ErrorMessage = "O Email é obrigatório.")]
         [StringLength(100, MinimumLength = 5, ErrorMessage = "O Email deve ter no mínimo 5 e no máximo 100 caracteres.")]
+        [EmailAddress(ErrorMessage = "O Email deve ser válido.")]
         public string Email { get; set; }
 
         //Idade
         [Display(Name = "Idade", Description = "A idade deve estar entre 15 e 100 anos.")]
         [Range(15, 100)]
         public int Idade { get; set; }
+
+        // Data de Nascimento
+        [Display(Name = "Data de Nascimento", Description = "Informe a data de nascimento.")]
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "A data de nascimento é obrigatória.")]
+        public DateTime DataNascimento { get; set; }
+
+        // Curso
+        [Display(Name = "Curso", Description = "Informe o curso que está matriculado.")]
+        [StringLength(100, ErrorMessage = "O campo curso não pode ter mais que 100 caracteres.")]
+        public string Curso { get; set; } // Novo parâmetro
     }
 }
